22844132df85dac3321f32414336a529f5903aa6
FerrandoCarlos <cferrando2015@gmail.com>
2024-04-09 16:38:17 -0300

    Inicio tp 5

--- src/controlador/Main.java
+++ src/controlador/Main.java
@@ -1,29 +1,14 @@
-package controlador;
+
+package Controlador;
 
 /**
- *
- * @author Martin
+ * @author Ferrando Carlos
  */
-
-/**
- *  Crea una aplicación GUI en Java que gestione una lista de productos. La interfaz debe contener los siguientes elementos:
-    Un ComboBox (JComboBox) que permita al usuario seleccionar una categoría de producto de una lista predefinida (por ejemplo, electrónica, ropa, alimentos, etc.).
-    Un botón "Agregar producto" (JButton) que permita al usuario agregar un nuevo producto a la lista.
-    Una tabla (JTable) que muestre los productos agregados, con columnas para el nombre del producto, la categoría y el precio.
-    Cuando el usuario seleccione una categoría en el ComboBox y haga clic en el botón "Agregar producto", el programa debe permitir al usuario ingresar el nombre y el precio del producto en campos de texto separados. Luego, debe agregar el producto a la tabla con la categoría seleccionada.
-    Además, la tabla debe ser actualizada automáticamente cada vez que se agrega un nuevo producto.
- */
-
 public class Main {
 
-    /**
-     * @param args the command line arguments
-     */
+   
     public static void main(String[] args) {
-        // TODO code application logic here
+        //Clonando el proyecto
     }
-    
-    
-    //METODO ESTATICO PARA SHOW DIALOGO
-    
+
 }
