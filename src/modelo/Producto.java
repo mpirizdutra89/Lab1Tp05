85abc4f8c425940a3c389a5f59661ecf15cdc960
mpirizdutra89 <163307010+mpirizdutra89@users.noreply.github.com>
2024-04-09 15:33:48 -0300

    Update Producto.java

--- src/modelo/Producto.java
+++ src/modelo/Producto.java
@@ -4,6 +4,9 @@
  */
 package modelo;
 
+
+//Cuando el usuario seleccione una categoría en el ComboBox y haga clic en el botón "Agregar producto", el programa debe permitir al usuario ingresar el nombre y el precio del producto en campos de texto separados. 
+//Luego, debe agregar el producto a la tabla con la categoría seleccionada.
 /**
  *
  * @author nn
