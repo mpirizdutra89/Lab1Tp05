965c836b57f5804d300d0f708217bb87c039c143
mpirizdutra89 <mpirizdutra@ulp.edu.ar>
2024-04-09 23:33:48 -0300

    Create Main.java

---
+++ src/controlador/Main.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package controlador;
+
+import vista.VistaGestionProducto;
+
+/**
+ *
+ * @author Martin
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        VistaGestionProducto vgp= new VistaGestionProducto();
+        vgp.setLocationRelativeTo(null);
+        vgp.setVisible(true);
+    }
+    
+}
