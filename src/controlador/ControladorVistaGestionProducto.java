ca8172bb41d3ac869bf7cc891b6180ffad085115
aguz11 <agus110196@gmail.com>
2024-04-10 00:28:14 -0300

    Update ControladorVistaGestionProducto.java
    Agregue método agregarProducto(),
    y metodo para vincular evento de agregar producto

--- src/controlador/ControladorVistaGestionProducto.java
+++ src/controlador/ControladorVistaGestionProducto.java
@@ -4,6 +4,8 @@
  */
 package controlador;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.util.ArrayList;
 import javax.swing.table.DefaultTableModel;
@@ -11,6 +13,7 @@ import modelo.Categorias;
 import modelo.EncabezadosTable;
 import modelo.Producto;
 import vista.VistaGestionProducto;
+import static controlador.Main.viewDialogo;
 
 /**
  *
@@ -32,7 +35,56 @@ public class ControladorVistaGestionProducto {
         };
         armarComboBx();
         armarCabeceraTabla();
+        vincularEventos();
     }
+    
+    //envento vinculado directamente desde el cod.
+    private static void vincularEventos() {
+        vgp.getJBTNAgregar().addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                agregarProducto();
+            }
+        });
+    }
+    
+    private static void agregarProducto() {
+        String nombre = vgp.getJTFNombre().getText();
+        String precioStr = vgp.getJTFPrecio().getText();
+
+        if (nombre.isEmpty() && precioStr.isEmpty()) {
+             viewDialogo("Se deben llenar todos los campos para continuar.",1);
+            return;
+        }
+
+        double precio;
+        try {
+            precio = Double.parseDouble(precioStr);
+        } catch (NumberFormatException ex) {
+            viewDialogo("Por favor, ingrese un precio valido.",1);
+            return;
+        }
+     String categoriaString = vgp.getJCBCategoria().getSelectedItem().toString();
+        if (categoriaString.equals("Seleccione rubro")) {
+            viewDialogo("Seleccione una categoria",1);
+            return;
+        }
+    
+    
+    Categorias categoria = Categorias.valueOf(categoriaString);
+
+        Producto nuevoProducto = new Producto(nombre, precio, categoria);
+        listaProducto.add(nuevoProducto);
+
+        
+        Object[] fila = {nombre, precio, categoria};
+        modeloTable.addRow(fila);
+
+        vgp.getJTFNombre().setText("");
+        vgp.getJTFPrecio().setText("");
+        vgp.getJCBCategoria().setSelectedIndex(0);
+    }
+    
     
     private static void armarComboBx(){
         
